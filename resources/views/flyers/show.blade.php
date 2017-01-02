15b86abf39484027a0e28be8e5cc8bd537bb7731
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-01-02 17:35:26 +0300

    Styling delete button

--- resources/views/flyers/show.blade.php
+++ resources/views/flyers/show.blade.php
@@ -22,19 +22,21 @@
 				<div class="row">
 					@foreach($set as $photo)
 						<div class="col-md-3 gallery_image">
-							<form method="POST" action="/photos/{{ $photo->id }}">
+							<div class="relative">
+								<form method="POST" action="/photos/{{ $photo->id }}" class="absolute">
+								
+									{!! csrf_field() !!}
+									
+									<input type="hidden" name="_method" value="DELETE" >
+									
+									<button class="deleteButton" type="submit">&times;</button>
+									
+								</form>
 							
-								{!! csrf_field() !!}
-								
-								<input type="hidden" name="_method" value="DELETE" >
-								
-								<button type="submit">Delete</button>
-								
-							</form>
-						
-							<a href="/{{ $photo->path }}" data-lity>
-								<img src="/{{ $photo->thumbnail_path }}" alt="">
-							</a>
+								<a href="/{{ $photo->path }}" data-lity>
+									<img src="/{{ $photo->thumbnail_path }}" alt="">
+								</a>
+							</div>
 						</div>
 					@endforeach
 				</div>	
